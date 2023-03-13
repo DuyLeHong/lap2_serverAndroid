a8dae6ad9c60cb861ae0909bfeed1810008d3274
Duy Le <lehongduy.bk@gmail.com>
2023-03-13 15:31:06 +0700

    update code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/tinhtoan", (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
   const soA = Number(req.body.soA);
   const soB = Number(req.body.soB);
